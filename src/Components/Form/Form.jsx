aa44b96732aa5ab14a6fd015b063d1308b0d564f
Tornike <chargeishvilitornike@gmail.com>
2025-03-05 19:13:52 +0400

    add input field

---
+++ src/Components/Form/Form.jsx
@@ -0,0 +1,14 @@
+import "./Form.scss";
+
+function Form() {
+    return (
+        <form>
+            <div class="input-container">
+                <div className="circle"></div>
+                <input type="text" />
+            </div>
+        </form>
+    );
+}
+
+export default Form;
